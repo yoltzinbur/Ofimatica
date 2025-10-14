453121775aeeaea6db2f3e5913433b76e8edd906
yoltzinbur <yoltzin.bur@gmail.com>
2025-10-13 23:20:57 -0600

    Agregar sección de navegación con React y Tailwindcss

---
+++ navegacion/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+    <div className="min-h-screen w-full bg-[#020617] relative">
+      {/* Fondo mamalón */}
+      <div className="absolute inset-0" style={{backgroundImage: `radial-gradient(circle 500px at 50% 200px, #3e3e3e, transparent)`,}}/>
+      {/* Contenido     */}
+      <div className="grid">
+        hola
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
