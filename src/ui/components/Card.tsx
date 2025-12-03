b1842fcc9d3ce75dd0d82311bd369e37f9e2a366
yoltzin.bur <yoltzin.bur@gmail.com>
2025-12-02 19:24:34 -0600

    Resolver problemitas de git

--- src/ui/components/Card.tsx
+++ src/ui/components/Card.tsx
@@ -2,21 +2,21 @@ import type { Card } from "../types/types";
 
 export default function Card({seccion, index, activeSlide, slideStyles, next, prev}: Card) {
   return (
-    <div className={`text-white relative ${slideStyles(index)} 
+    <div className={`text-white relative ${activeSlide === index ? 'z-20' : 'z-10'} ${slideStyles(index)} 
     ${(index === 0 && activeSlide === 0) ? 'col-start-2' : 'col-start-auto' }`}>
         <figure className='aspect-[4/5] h-[460px] mask-b-from-50% mask-b-to-90%'>
            <img src={seccion.img} alt={`Imagen de la sección ${seccion.title}`} className='size-full object-cover' />
         </figure>
-        <div className={`absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/3
+        <div className={`absolute bottom-0 left-1/2 -translate-x-1/2 sm:translate-y-1/3 translate-y-20
         md:w-3xl px-5 text-center flex flex-col justify-center items-center gap-2
         ${index === activeSlide ? '' : 'hidden'}`}>
-            <h2 className='text-5xl font-black tracking-widest uppercase'>
+            <h2 className='md:text-5xl text-4xl font-black tracking-widest uppercase'>
                 {seccion.title}
             </h2>
-            <p className='text-lg text-wrap'>
+            <p className='md:text-lg text-wrap'>
                 {seccion.content}
             </p>
-            <div className='flex gap-12'>
+            <div className='flex md:gap-12 gap-6'>
                 <button className='bg-zinc-900 hover:bg-stone-800 transition-all duration-300 px-5 rounded-lg
                 cursor-pointer shadow-lg/50 font-bold'
                 onClick={prev}>
