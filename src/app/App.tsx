1a570e0fbc9c9b788e0351c604546d0b992c7517
yoltzinbur <yoltzin.bur@gmail.com>
2026-01-09 11:04:52 -0600

    Cambiar HashRouter por BrowserRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
 import Nav from "../ui/components/Nav"
-import { Routes, Route, HashRouter } from "react-router-dom"
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Index from "../ui/components/Index"
 
 function App() {
 
   return (
     <>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Index /> }></Route>
         <Route path="/navegacion" element={ <Nav /> }></Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </>
   )
 }
